c4d64eaf8c3b77bbaf806a6fd1d865fbea5dc156
Thomas Clement Mogensen <mogensen@diku.dk>
2013-01-02 13:20:10 +0100

    import bug

--- src/collective/traceview/patches/viewlets.py
+++ src/collective/traceview/patches/viewlets.py
@@ -5,7 +5,7 @@ def extract_name(func, f_args, f_kwargs, res):
     kv = {'Name': f_args[0].__name__}
     return kv
 
-from plone.app.layout.viewlets.manager import ViewletBase
+from plone.app.layout.viewlets.common import ViewletBase
 ViewletBase.orig_render = ViewletBase.render
 vm_wrapper = oboe.profile_function('plone_viewlet', callback=extract_name)
 ViewletBase.render = vm_wrapper(ViewletBase.orig_render)
