51688ac66085a6ec398737093d0d1b4a999fe8b9
Anton Stonor <anton@headnet.dk>
2013-07-22 15:28:30 +0200

    apply adjusted zodb reporting as suggestion in issue #6. DB name hardcoded for now

--- src/collective/traceview/patches/zodbpatch.py
+++ src/collective/traceview/patches/zodbpatch.py
@@ -7,19 +7,20 @@ def extract_obj_info(func, f_args, f_kwargs):
     obj = f_args[1]
 
     # lets mimic a mongodb flavor query so queries will show up in the list
-    query = '{"Class":"%s","OID":%s}' % (str(obj.__class__), u64(obj._p_oid))
-
+    query = '{"OID":%s}' % u64(obj._p_oid)
+    
     kv =  {'Flavor': 'mongodb',
-           'QueryOp': 'find',
+           'Database': 'main',
+           'Collection': obj.__class__.__name__,
            'Query': query
           }
 
     return f_args, f_kwargs, kv
 
 Connection.orig_setstate = Connection.setstate
-ss_wrapper = oboe.log_method('zodb', entry_kvs={'Op': 'setstate'}, before_callback=extract_obj_info)
+ss_wrapper = oboe.log_method('zodb', entry_kvs={'QueryOp': 'setstate'}, before_callback=extract_obj_info)
 Connection.setstate = ss_wrapper(Connection.orig_setstate)
 
-commit_wrapper = oboe.log_method('zodb', entry_kvs={'Op': 'commit'})
+commit_wrapper = oboe.log_method('zodb', entry_kvs={'QueryOp': 'commit'})
 Connection.orig_commit = Connection.commit
 Connection.commit = commit_wrapper(Connection.orig_commit)
