c64006787a4d4e3023ad536f8063dc7d492e503c
Anton Stonor <anton@headnet.dk>
2013-07-23 11:21:40 +0200

    extract id and title of object where available

--- src/collective/traceview/patches/zodbpatch.py
+++ src/collective/traceview/patches/zodbpatch.py
@@ -3,22 +3,35 @@ from ZODB.Connection import Connection
 from ZODB.utils import u64
 
 
-def extract_obj_info(func, f_args, f_kwargs):
+def build_query(func, f_args, f_kwargs):
     obj = f_args[1]
 
     # lets mimic a mongodb flavor query so queries will show up in the list
     query = '{"OID":%s}' % u64(obj._p_oid)
     
-    kv =  {'Flavor': 'mongodb',
+    kv = {'Flavor': 'mongodb',
            'Database': 'main',
            'Collection': obj.__class__.__name__,
            'Query': query
-          }
+         }
 
     return f_args, f_kwargs, kv
 
+
+def extract_obj_info(func, f_args, f_kwargs, res):
+    kv = {}
+    obj = f_args[1]
+    if hasattr(obj, 'id'):
+        kv['obj.id'] = obj.id
+    if hasattr(obj, 'title'):
+        kv['obj.title'] = obj.title
+
+    return kv
+
 Connection.orig_setstate = Connection.setstate
-ss_wrapper = oboe.log_method('zodb', entry_kvs={'QueryOp': 'setstate'}, before_callback=extract_obj_info)
+ss_wrapper = oboe.log_method('zodb', entry_kvs={'QueryOp': 'setstate'},
+                             before_callback=build_query, 
+                             callback=extract_obj_info)
 Connection.setstate = ss_wrapper(Connection.orig_setstate)
 
 commit_wrapper = oboe.log_method('zodb', entry_kvs={'QueryOp': 'commit'})
