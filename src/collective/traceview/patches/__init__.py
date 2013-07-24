7b96063cbf83805d70c87a87781dc9b16b13dff9
Anton Stonor <anton@headnet.dk>
2013-07-24 10:39:14 +0200

    try to handle both Plone 3 and Plone 4

--- src/collective/traceview/patches/__init__.py
+++ src/collective/traceview/patches/__init__.py
@@ -1,19 +1,27 @@
+import sys
+if sys.version_info >= (2, 6):
+    is_recent_python = True
+else:
+    is_recent_python = False
+    
 try:
     import oboeexception
     from oboeware import loader
-    loader.load_inst_modules()
+    if is_recent_python:
+        loader.load_inst_modules()
 except:
     import logging
     logger = logging.getLogger("Plone")
     logger.warn("Could not import oboeware, no tracing is done.")
     pass
 else:
-    #import httpserver
     import publisher
     import catalog
-    import transform
     import zodbpatch
     import memoize
     import viewletmanagers
     #import template
+    if is_recent_python:
+        import transform
+    
 
